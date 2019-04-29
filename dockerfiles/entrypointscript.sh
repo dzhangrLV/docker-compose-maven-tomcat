97b02a81cef500b28c4362812b2db21bcafc65aa
leonid.gorshkov <dzhangr.lv@gmail.com>
2019-04-29 17:23:54 +0300

    up version 8

--- dockerfiles/entrypointscript.sh
+++ dockerfiles/entrypointscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mvn clean package -e -f  /usr/src/mymaven/boxfuse-sample-java-war-hello  &&
-find /usr/src/mymaven/target -name "*.war" -exec cp '{}' /usr/local/tomcat/webapps \;
+find /usr/src/mymaven/boxfuse-sample-java-war-hello/target -name "*.war" -exec cp '{}' /usr/local/tomcat/webapps \;
