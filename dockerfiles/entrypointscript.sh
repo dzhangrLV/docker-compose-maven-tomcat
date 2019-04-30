dad7ac5b3f3971f789666fc2e5e1736f0db1e13f
leonid.gorshkov <dzhangr.lv@gmail.com>
2019-04-30 14:21:52 +0300

    some optimization

--- dockerfiles/entrypointscript.sh
+++ dockerfiles/entrypointscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn clean package -e -f  /usr/local/bin/boxfuse-sample-java-war-hello  &&
+mvn clean install -e -f  /usr/local/bin/boxfuse-sample-java-war-hello  &&
 find /usr/local/bin/boxfuse-sample-java-war-hello/target -name "*.war" -exec cp '{}' /usr/local/tomcat/webapps \;
